231fe3df3271e03836a8423bbda343d8f71258a4
Anis HADJARI <anis_sg@hotmail.com>
2019-04-03 19:11:45 +0100

    -first commit -solution of the problem #1

---
+++ Problem001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 10, 15, 3, 7 };
+            int k = 17;
+
+            Console.WriteLine(UpToK(input, k));
+
+            input = new int[] { 5, 12, 33, 2, 11, 48, 15, 3, 9 };
+            k = 22;
+
+            Console.WriteLine(UpToK(input, k));
+        }
+
+        private static bool UpToK(int[] input, int k)
+        {
+            HashSet<int> rest = new HashSet<int>();
+            foreach (int n in input)
+            {
+                if (rest.Contains(k - n))
+                    return true;
+                rest.Add(n);
+            }
+            return false;
+        }
+    }
+}
